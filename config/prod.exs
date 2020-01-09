ad3db4d3abe2a0d90fa371cc46a72dbd340261af
Anton Shubin <signal2564@gmail.com>
2020-01-09 18:59:27 +0300

    change url app name

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :drunkard, DrunkardWeb.Endpoint,
-  url: [host: System.get_env("APP_NAME") <> ".shaddy.space", port: 4000],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
